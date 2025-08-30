552fd79b9e5c8983f7406bc6827d36de39fe1819
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2025-08-30 15:31:11 +0530

    fix: add verify command to commands list lmao

--- src/slash-commands/index.ts
+++ src/slash-commands/index.ts
@@ -1,6 +1,8 @@
 import type { SlashCommand } from '@/types/command';
 import ping from './ping';
+import verify from './verify';
 
 export default [
     ping,
+    verify,
 ] satisfies SlashCommand[];
